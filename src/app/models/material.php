9d6fbf0c8b1cc974213ebfc8f348816244e84e71
Boriss Mejias <tchorix@gmail.com>
2011-10-25 20:02:17 +0200

    add function to retrieve the titlte of a book associated to a given material

--- src/app/models/material.php
+++ src/app/models/material.php
@@ -3,6 +3,23 @@
 class Material extends AppModel
 {
 	var $name = 'Material';
+
+	/**
+	 * Find the title of the book associated with the material_id
+	 *
+	 * @param serial $material_id 
+	 * @return string book's title
+	 */
+	function get_book_title($material_id)
+	{
+		$query = "select books.title from books where books.id = ";
+		$query.= "(select book_id from materials where id = ".$material_id.")";
+		$result = $this->query($query);
+		if (count($result) > 0)
+			return $result[0][0]['title'];
+		else
+			return "ERROR - book not found";
+	}
 }
 
 ?>
