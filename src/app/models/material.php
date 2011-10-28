25102f1a5a015ea222627bdc445c6a45bf5d5309
Boriss Mejias <tchorix@gmail.com>
2011-10-28 20:15:32 +0200

    changing get_book_title by get_book_info where books.id and books.title are returned in an array

--- src/app/models/material.php
+++ src/app/models/material.php
@@ -5,20 +5,24 @@ class Material extends AppModel
 	var $name = 'Material';
 
 	/**
-	 * Find the title of the book associated with the material_id
+	 * Find the title and id of the book associated with the material_id
 	 *
 	 * @param serial $material_id 
-	 * @return string book's title
+	 * @return array with book's title and book's id.
 	 */
-	function get_book_title($material_id)
+	function get_book_info($material_id)
 	{
-		$query = "select books.title from books where books.id = ";
+		$query = "select books.id, books.title from books where books.id = ";
 		$query.= "(select book_id from materials where id = ".$material_id.")";
 		$result = $this->query($query);
+		//echo "This is what I got from the query\n".print_r($result, true);
+		$to_return = array('id' => 0, 'title' => "ERROR - book not found");
 		if (count($result) > 0)
-			return $result[0][0]['title'];
-		else
-			return "ERROR - book not found";
+		{
+			$to_return['id']	= $result[0][0]['id'];
+			$to_return['title']	= $result[0][0]['title'];
+		}
+		return $to_return;
 	}
 }
 
