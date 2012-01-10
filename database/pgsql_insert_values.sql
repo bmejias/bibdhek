989683bc646a9af5cc6563efa9e53a55e09f1505
Boriss Mejias <tchorix@gmail.com>
2012-01-10 17:55:00 +0100

    adding new bib_admin user

--- database/pgsql_insert_values.sql
+++ database/pgsql_insert_values.sql
@@ -4,6 +4,9 @@ INSERT INTO groups (name)
 INSERT INTO users (username, first_name, last_name, email, password)
 	VALUES ('saartje', 'Saartje', 'Renaers', 'saartje@renaers.be', 'topsecret');
 
+INSERT INTO users (username, first_name, last_name, email, password)
+	VALUES ('bib_admin', 'Admin', 'van de Bib', 'noreply@bibdhek.be', 'secret');
+
 INSERT INTO group_users (user_id, group_id)
 	VALUES ((SELECT id FROM groups WHERE name='admin'),
 			(SELECT id FROM users WHERE username='saartje'));
