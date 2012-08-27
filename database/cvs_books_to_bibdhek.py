45e8c9f9b5dcee5d603226034b4c76ab49351ae8
Boriss Mejias <tchorix@gmail.com>
2012-08-28 01:34:23 +0200

    the script to parse a csv with, and insert them into the database is working nicely

--- database/cvs_books_to_bibdhek.py
+++ database/cvs_books_to_bibdhek.py
@@ -27,9 +27,37 @@ def cd_and_title(book_title):
         with_cd = True
     else:
         clean_title = book_title.strip()
-    print "cd %r" % with_cd
-    print "title " + clean_title
+    # print "cd %r" % with_cd
+    # print "title " + clean_title
     return with_cd, clean_title
+
+def csv_to_dictionary(books_csv, lang):
+    """Read books csv file and return dictionary with data to insert."""
+    books = {}
+    next(books_csv)
+    for line in books_csv:
+        book        = line.split(';')
+        level       = read_value(book[LEVEL])
+        collection  = read_value(book[COLLECTION])
+        author      = read_value(book[AUTHOR])
+        full_title  = read_value(book[TITLE])
+        (cd, title) = cd_and_title(full_title)
+        acquired    = read_value(book[ACQUIRED])
+        key         = "%s %s" % (author, title)
+        key         = key.lower()
+
+        if books.has_key(key):
+            books[key]['copies'] += 1
+        else:
+            books[key] = {'author'  : author,
+                          'title'   : title,
+                          'cd'      : cd,
+                          'collection' : collection,
+                          'level'   : level,
+                          'acquired': acquired,
+                          'lang'    : lang,
+                          'copies'  : 1}
+    return books
 
 # Get language if provided. Set empty otherwise
 if len(sys.argv) >= 3:
@@ -37,61 +65,43 @@ if len(sys.argv) >= 3:
 else:
     lang = ''
 
-# Read books csv file and return dictionary with data to insert
-books_csv   = open(sys.argv[1])
-books       = {}
+books_csv = open(sys.argv[1])
+print "Parsing csv file"
+books = csv_to_dictionary(books_csv, lang)
 
-for line in books_csv:
-    book        = line.split(';')
-    level       = read_value(book[LEVEL])
-    collection  = read_value(book[COLLECTION])
-    author      = read_value(book[AUTHOR])
-    full_title  = read_value(book[TITLE])
-    (cd, title) = cd_and_title(full_title)
-    acquired    = read_value(book[ACQUIRED])
-    key         = "%s %s" % (author, title)
-    key         = key.lower()
-    print "KEY " +key
+try:
+    print "Establishing connection to the database"
+    conn = psycopg2.connect(dbname="bibdhek",
+                            host="localhost", port="5433",
+                            user="bd_admin", password="bd_admin")
+except Exception, e:
+    print "ERROR: I am unable to connect to the database"
+    sys.exit(1)
 
-    if books.has_key(key):
-        books[key]['copies'] += 1
-    else:
-        books[key] = {'author'  : author,
-                      'title'   : title,
-                      'cd'      : cd,
-                      'collection' : collection,
-                      'level'   : level,
-                      'acquired': acquired,
-                      'language': lang,
-                      'copies'  : 1}
+cursor = conn.cursor()
 
-print books
-
-# Connect to the database
-# try:
-#     conn = psycopg2.connect(dbname="bibdhek_test",
-#                             host="localhost", port="5433",
-#                             user="bd_admin", password="bd_admin")
-# except Exception, e:
-#     print "I am unable to connect to the database"
-# 
-# cursor = conn.cursor()
-# cursor.execute("SELECT datname from pg_database")
-# rows = cursor.fetchall()
-# 
-# print "\nShow me the databases:\n"
-# for row in rows:
-#     print "    ", row[0]
-# 
-# cursor.execute("INSERT INTO foo (title) VALUES (%s) RETURNING id",
-#                ("This is foo",))
-# new_foo = cursor.fetchone()[0]
-# 
-# cursor.execute("INSERT INTO bar (foo_id, status) VALUES (%s, %s)",
-#                (new_foo, "ready"))
-# conn.commit()
-# 
-# cursor.close()
-# conn.close()
-
+print "Inserting values"
+try:
+    for key in books:
+        book = books[key]
+        insert_book = """
+            INSERT INTO books
+            (title, author, cd, collection, level, lang, acquired)
+            VALUES (%(title)s, %(author)s, %(cd)s, %(collection)s,
+                    %(level)s, %(lang)s, %(acquired)s)
+            RETURNING id"""
+        cursor.execute(insert_book, book)
+        new_book = cursor.fetchone()[0]
+        for i in range(1, book['copies'] + 1):
+            insert_copy = """INSERT INTO copies (book_id, status)
+                            VALUES (%s, 'available')""" % new_book
+            cursor.execute(insert_copy)
+        conn.commit()
+except Exception, e:
+    conn.rollback()
+    raise e
+finally:
+    print "Done... closing connection"
+    cursor.close()
+    conn.close()
 
