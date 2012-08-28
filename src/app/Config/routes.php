51f6e50dbc60bb4931b8647a01cbe172db469250
Boriss Mejias <tchorix@gmail.com>
2012-08-29 00:42:48 +0200

    changing default page

--- src/app/Config/routes.php
+++ src/app/Config/routes.php
@@ -25,7 +25,10 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'users', 'action' => 'login'));
+    // All default login page.
+	// Router::connect('/', array('controller' => 'users', 'action' => 'login'));
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display',
+                            'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
