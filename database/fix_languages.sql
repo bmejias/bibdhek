43cc737600778c73df54ea53181076ab93da92f5
Boriss Mejias <tchorix@gmail.com>
2013-12-31 00:35:47 +0100

    adding sql query to fix the languages in production database... the wrong value was entered via cvs import

---
+++ database/fix_languages.sql
@@ -0,0 +1,5 @@
+UPDATE books SET lang = 'nl' WHERE lang = 'N';
+UPDATE books SET lang = 'fr' WHERE lang = 'F';
+UPDATE books SET lang = 'en' WHERE lang = 'E';
+UPDATE books SET lang = 'de' WHERE lang = 'D';
+UPDATE books SET lang = 'nl' WHERE lang = '';
